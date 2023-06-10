daa8a4b4cb5c0a27b315b75b0c0a04ad390ea0d1
Kostja P <kpavlovsky@users.noreply.github.com>
2023-06-10 22:58:54 +0200

    when clicking on create a new building and we already creating then delete not only script component, but also the whole gameobject of the new building

--- Assets/Scripts/paintbrush.cs
+++ Assets/Scripts/paintbrush.cs
@@ -53,7 +53,7 @@ public class paintbrush : MonoBehaviour
     {
         if (building != null)
         {
-            Destroy(building);
+            Destroy(building.gameObject);
         }
     }
 
