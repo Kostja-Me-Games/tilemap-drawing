000983a1e34772e152ad9cb8659e77c9df4d9133
Kostja P <kpavlovsky@users.noreply.github.com>
2023-06-13 12:25:30 +0200

    scroll respects size of the panel, also scroll speed is based on the zoom level

--- Assets/ScrollMap.cs
+++ Assets/ScrollMap.cs
@@ -8,6 +8,9 @@ public class ScrollMap : MonoBehaviour
     // Start is called before the first frame update
     public float scrollSpeed = 0.0f;
     public float scrollZoneSize = 50f;
+    public MouseOverPanel mouseOverPanel;
+    public zoom zoom;
+    public float panelWidth = 0.0f;
     void MoveCamera(Vector3 direction)
     {
         Vector3 newPosition = transform.position + direction * scrollSpeed * Time.deltaTime;
@@ -21,14 +24,26 @@ public class ScrollMap : MonoBehaviour
         );
         transform.position = clampedPosition;
     }
+
+    private void UpdateScrollSpeed()
+    {
+        scrollSpeed = 2 + zoom.zoomSize;
+    }
+
     void Start()
     {
-        
+        panelWidth = mouseOverPanel.GetComponent<RectTransform>().rect.width;
+        zoom = GetComponent<zoom>();
+        UpdateScrollSpeed();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!mouseOverPanel.isMouseOver)
+        {
+            UpdateScrollSpeed();
+        
         Vector3 mousePosition = Input.mousePosition;
 
         // Check if mouse position is within the scroll zone
@@ -36,7 +51,7 @@ public class ScrollMap : MonoBehaviour
         {
             MoveCamera(Vector3.left);
         }
-        else if (mousePosition.x > Screen.width - scrollZoneSize)
+        else if (mousePosition.x > Screen.width - scrollZoneSize - panelWidth)
         {
             MoveCamera(Vector3.right);
         }
@@ -48,6 +63,7 @@ public class ScrollMap : MonoBehaviour
         else if (mousePosition.y > Screen.height - scrollZoneSize)
         {
             MoveCamera(Vector3.up);
+        }
         }
     }
 }
