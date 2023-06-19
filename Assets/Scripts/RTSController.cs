d9046d6e998a638f2776113cbd7c116add0827b5
Kostja P <kpavlovsky@users.noreply.github.com>
2023-06-19 18:23:39 +0200

    unit selection done

--- Assets/Scripts/RTSController.cs
+++ Assets/Scripts/RTSController.cs
@@ -7,21 +7,39 @@ public class RTSController : MonoBehaviour
 
 public Vector3 startPosition;
 [SerializeField] private List<RTSUnit> selectedRTSUnitList;
+[SerializeField] private Transform selectionAreaTransform;
 private void Awake() {
     selectedRTSUnitList = new List<RTSUnit>();
+    selectionAreaTransform.gameObject.SetActive(false);
 }
 void Update()
 {
  if (Input.GetMouseButtonDown(0))
  {
     // Left Mouse Button Pressed
+    selectionAreaTransform.gameObject.SetActive(true);
     startPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
  }
 
+ if (Input.GetMouseButton(0)) {
+     // Left Mouse Button Held Down
+     Vector3 currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+     Vector3 lowerLeft = new Vector3(
+         Mathf.Min(startPosition.x, currentMousePosition.x),
+         Mathf.Min(startPosition.y, currentMousePosition.y)
+     );
+     Vector3 upperRight = new Vector3(
+         Mathf.Max(startPosition.x, currentMousePosition.x),
+         Mathf.Max(startPosition.y, currentMousePosition.y)
+     );
+     selectionAreaTransform.position = lowerLeft;
+     selectionAreaTransform.localScale = upperRight - lowerLeft;
+ }
 
  if (Input.GetMouseButtonUp(0))
  {
      // Left mouse button held down
+     selectionAreaTransform.gameObject.SetActive(false);
      Collider2D[] collider2DArray = Physics2D.OverlapAreaAll(startPosition, Camera.main.ScreenToWorldPoint(Input.mousePosition));
      foreach (RTSUnit rtsUnit in selectedRTSUnitList)
      {
