be6b734402a5498901691e368ab33af3d06f0ae7
Kostja P <kpavlovsky@users.noreply.github.com>
2023-06-29 21:05:43 +0200

    debugging the problem with units taking over the same tile

--- Assets/Scripts/RTSUnit.cs
+++ Assets/Scripts/RTSUnit.cs
@@ -57,54 +57,14 @@ public class RTSUnit : MonoBehaviour
         path = null;
         currentPathIndex = 0;
     }
-    public void _MoveTo(Vector3 endPos) {
-        endPosition = endPos;
-        startPosition = transform.position;
-        endPosition.z = 0;
-        Vector2 vectorToTarget = endPosition - startPosition;
-        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - 90;
-        spriteTransform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-    }
+
     public void TurnToTargetPosition(Vector3 target) {
         startPosition = transform.position;
         Vector2 vectorToTarget = target - startPosition;
         float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - 90;
         spriteTransform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
-    public void StartMoving() {
-        // start moving towards the end position
 
-        
-        // save the new position in a new variable and check if the building tilemap tile is empty
-        Vector3 nextPosition = Vector3.MoveTowards(transform.position, endPosition, 2 * Time.deltaTime);
-        Vector3Int nextTilePosition = pb.WorldToCell(nextPosition);
-        BoundsInt nextArea = new BoundsInt(nextTilePosition.x, nextTilePosition.y, 0, 1, 1, 1);
-        bool nextAreaIsDifferent = !nextArea.Equals(area);
-        if (nextAreaIsDifferent) {
-            // we need to check if unit can step on the next tile
-            bool canMoveToNextArea = pb.AreaCanBeTravelled(nextArea);
-            if (!canMoveToNextArea) {
-                // if the tile is not empty, stop moving
-                SetMoving(false);
-                endPosition = transform.position;
-                return;
-            }
-            // if the tile is empty, move the unit to the new position
-            transform.position = nextPosition;
-            
-            
-        } else {
-            // it is still the same tile, so just keep moving
-            transform.position = nextPosition;
-            
-        }
-        if (transform.position == endPosition)
-        {
-            SetMoving(false);
-        } else {
-            SetMoving(true);
-        }
-    }
 
     public void SetMoving(bool moving) {
         // access animator of the child object "Sprite" and set "Moving" parameter to true
@@ -184,10 +144,6 @@ public class RTSUnit : MonoBehaviour
     void Update()
     {
         MoveAlongPath();
-        // if (transform.position != endPosition && endPosition != Vector3.zero)
-        // {
-        //     StartMoving(); 
-        // }
         SetMoving(isMoving);
         UpdateTakenArea();
     }
